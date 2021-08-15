3d18c26a8a6e750d17e9ff4033218fe123e03524
Karthi <karthiraja268@gmail.com>
2021-08-15 23:49:53 +0530

    profile card updated

--- src/Profile.js
+++ src/Profile.js
@@ -7,14 +7,14 @@ function Profile(props) {
     const user = userData[props.match.params.id];
 
     return (
-        <div className="profile-container row mt-3">
-            <div className="card text-dark bg-info mb-3 col-12 profile-container p-3" style={{ maxWidth: "25rem" }}>
-                <img className="image-container mb-4" src={user.imgurl} alt="profile pic" height="200" width="200" />
+        <div className="profile-container row mt-1">
+            <div className="card text-dark bg-info mb-2 col-12 profile-container p-3" style={{ maxWidth: "25rem" }}>
+                <img className="image-container mb-3" src={user.imgurl} alt="profile pic" height="200" width="200" />
                 <div className="card-header card-name"><h4>{user.name}</h4></div>
                 <div className="card-body">
                     <h5 className="card-mail">{user.email}</h5>
                     <h5 className="card-age">{user.age}</h5>
-                    <p className="card-desc">{user.description}</p>
+                    <div className="card-desc">{user.description}</div>
                 </div>
                 <Link to={`/edit-profile/${props.match.params.id}`} className="btn btn-outline-primary">Edit profile</Link>
             </div>
--- src/UserContext.js
+++ src/UserContext.js
@@ -11,7 +11,7 @@ const UsersData = [
         age: 25,
         email: 'person1@gmail.com',
         imgurl: 'https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-        description: "Lorem ipsum"
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem corrupti sapiente mollitia velit eaque molestias ut voluptatibus odit, rem incidunt."
     },
     {
         id: 2,
@@ -19,7 +19,7 @@ const UsersData = [
         age: 38,
         email: 'person2@gmail.com',
         imgurl: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-        description: 'Lorem Ipsum'
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur laboriosam asperiores nihil, ipsa voluptatibus quos'
     }
 ]
 
